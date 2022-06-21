ecd08b3b7a7323738637bdb1647e39328f5145aa
lukoscodes <plukosevic@gmail.com>
2022-06-21 08:00:50 +0100

    Create PhraseOMatic.java

---
+++ src/PhraseOMatic.java
@@ -0,0 +1,22 @@
+public class PhraseOMatic {
+    public static void main(String[] args) {
+
+        String[] wordListOne = {"agnostic", "opinionated", "voice activated", "haptically driven", "extensible", "reactive", "agent based", "functional", "AI enabled", "Strongly typed"};
+        String[] wordListTwo = {"event drive", "pub-sub", "IoT", "cloud native", "service oriented", "containerized", "serverless", "microservices", "distributed ledger"};
+        String[] wordListThree = {"framework", "library","DSL", "REST API", "repository", "pipeline", "service mesh", "architecture", "perspective", "design", "orientation"};
+
+        int oneLength = wordListOne.length;
+        int twoLength = wordListTwo.length;
+        int threeLength = wordListThree.length;
+
+        java.until.Random randomGenerator = new java.until.Random();
+        int rand1 = randomGenerator.nextInt(oneLength);
+        int rand2 = randomGenerator.nextInt(twoLength);
+        int rand3 = randomGenerator.nextInt(threeLength);
+
+        String phrase = wordListOne[rand1] + " " + wordListTwo[rand2] + " " + wordListThree[rand3];
+
+        System.out.println("What we need is a" + phrase);
+
+    }
+}
