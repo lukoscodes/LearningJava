069d41e9bdd74661bccaabd1e0a604ca47efe07a
lukoscodes <plukosevic@gmail.com>
2022-06-21 07:34:02 +0100

    Learning Java 21st of June 2022

---
+++ src/studentage.java
@@ -0,0 +1,15 @@
+public class studentage {
+    public static void main(String[] args) {
+         int studentAge1 = 15;
+         double studentgpa = 3.45;
+         char studentfirstinitial = 'K';
+         char studentlastinitial = 'H';
+         boolean hasPerfecAattendence = true;
+         String studentName ="Pulius Lukosevicius";
+         System.out.println(studentAge1);
+         System.out.println(studentgpa);
+         System.out.println(studentfirstinitial);
+         System.out.println(studentlastinitial);
+         System.out.println(hasPerfecAattendence);
+    }
+}
