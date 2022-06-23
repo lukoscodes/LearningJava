7ff8dfb995350a84adba5bd200c0d60a20d01bd3
lukoscodes <plukosevic@gmail.com>
2022-06-23 07:25:57 +0100

    Training

---
+++ src/MealPrice.java
@@ -0,0 +1,21 @@
+public class MealPrice {
+    public static double calculateTotalMealPrice(double listedMealPrice,
+                                               double tipRate,
+                                               double taxRate) {
+        double tip = tipRate * listedMealPrice;
+        double tax = taxRate * listedMealPrice;
+        double result = listedMealPrice + tip + tax;
+        System.out.println("Your total meal price is " + result);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        double groupTotalMealPrice = calculateTotalMealPrice(100, .2, .08);
+        System.out.println(groupTotalMealPrice);
+
+        double IndividualMealPrice = groupTotalMealPrice / 5;
+        System.out.println(IndividualMealPrice);
+
+
+    }
+}
